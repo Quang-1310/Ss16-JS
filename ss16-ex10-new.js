72d8e12fc714d4cacbe931e21672356f38759b23
Quang-1310 <hoangminhquang1310@gmail.com>
2025-02-26 14:01:30 +0700

    Delete ss16-ex10-new.js

--- ss16-ex10-new.js
+++
@@ -1,8 +0,0 @@
-let number1 = parseInt(prompt("Nhập vào số thứ nhất"));
-let number2 = parseInt(prompt("Nhập vào số thứ hai"));
-
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-alert("Số ngẫu nhiên: " + getRndInteger(number1, number2));
