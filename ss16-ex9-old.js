b3c37bc4c64ecabd6da324d8a092d978f804eec0
Quang-1310 <hoangminhquang1310@gmail.com>
2025-02-26 11:52:55 +0700

    Delete ss16-ex9-old.js

--- ss16-ex9-old.js
+++
@@ -1,9 +0,0 @@
-let string = prompt("Nhập chuỗi: ");
-
-string = string[0].toUpperCase() + string.slice(1);
-
-for(let i=1; i<string.length; i++){
-    string = string.slice(0,i) + string[i].toLowerCase() + string.slice(i+1);
-}
-
-alert(string);
